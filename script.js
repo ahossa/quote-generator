e5fc0bf9b6129c0edaa22e0ca005ba6c5f7a8736
Asif Bin Hossain <a.evan17@gmail.com>
2020-06-29 21:17:08 -0400

    Initial Commit & Full working project

--- script.js
+++ script.js
@@ -1,1 +1,71 @@
-console.log("testing");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Method to get quote from the API
+async function getQuote() {
+  showLoadingSpinner();
+
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+
+    // When the Author-Text field is blank, add "Unknown" to it
+    if (data.authorText === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    // Dynamically decrease the size for long Quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+
+    // stop showing the loader and show quote now
+    // That we've processed everything
+    showQuoteContainer();
+  } catch (error) {
+    // @TODO : This is a recursive code, should have exit conditions
+
+    getQuote();
+    console.log(error);
+  }
+}
+
+// Show Loading Icon for delayed Fetch
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function showQuoteContainer() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event-Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On Load
+getQuote();
